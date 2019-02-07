bf83819537aa709f7143caedcfac2084bc974de2
Brandon Siegel <brandon.siegel@microsoft.com>
2019-02-07 13:59:38 -0800

    Implement a naive list manipulation operation
    In this case we used a list structure where all nodes are on the heap,
    next time we will change to using a Box and see how that changes things.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
-enum Node<T> {
-    Data(T, Box<Self>),
+use std::fmt;
+
+enum Node<'a, T> {
+    Data(T, &'a Self),
     Empty
 }
 
-fn main() {
-    let data: Node<u8> = Node::Data(2, Box::new(Node::Empty));
-    {
-        let data2: Node<u8> = Node::Data(2, Box::new(data));
-        let data3: Node<u8> = Node::Data(3, Box::new(data));
+impl<'a, T: fmt::Display> fmt::Display for Node<'a, T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Node::Empty => write!(f, "empty"),
+            Node::Data(h,t) => write!(f, "{}, {}", h, t)
+        }
     }
-    
+}
+
+fn prepend<'a, T>(list: &'a Node<T>, value: T) -> Node<'a, T> {
+    match list {
+        Node::Data(_,_) => Node::Data(value, list),
+        Node::Empty => Node::Data(value, &Node::Empty)
+    }
+}
+
+fn main() {
+    let data: Node<u8> = Node::Data(2, &Node::Empty);
+    let data2 = prepend(&data, 3);
+    println!("{}", data2);
 }
